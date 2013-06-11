eb326c9cd7c27021eb6532be3d3d76fe12f3cc29
Gregory Koberger <gkoberger@gmail.com>
2013-06-11 01:08:49 -0700

    Clean up JS

---
+++ source/js/all.js
@@ -0,0 +1,3 @@
+//= 'require "vendor/jquery.lightbox_me.js"'
+//= require "plugins.js"
+//= require "main.js"
--- source/js/plugins.js
+++ source/js/plugins.js
@@ -12,3 +12,20 @@ if (!(window.console && console.log)) {
 }
 
 // Place any jQuery/helper plugins in here.
+
+$.fn.serializeObject = function(){
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name]) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
